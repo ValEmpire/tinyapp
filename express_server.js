e57261f2b5cde7ad4c72da7dd7349f1e82b5dc21
Arcival Palma <palma_arcival@yahoo.com>
2021-08-25 19:43:55 +0000

    setup single url page. getting the params

--- express_server.js
+++ express_server.js
@@ -13,23 +13,17 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: 'Hello World!' };
-  res.render("hello_world", templateVars);
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
+app.get("/urls/:shortURL", (req, res) => {
+
+  const { shortURL } = req.params;
+
+  const templateVars = { shortURL: shortURL, longURL: urlDatabase[shortURL] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
